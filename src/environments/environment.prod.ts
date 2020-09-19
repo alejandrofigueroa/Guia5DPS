60d99d78b9fe98e2d6d3385516ae7ab6b085a98d
alejandrofigueroa <alejandrofigueroa416@gmail.com>
2020-09-19 16:50:55 -0600

    Primer commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDGb5VA1pgtTKdPyLVWeiWFyqd9o75RJNM",
+    authDomain: "crudangularfirebase-8b2dd.firebaseapp.com",
+    databaseURL: "https://crudangularfirebase-8b2dd.firebaseio.com",
+    projectId: "crudangularfirebase-8b2dd",
+    storageBucket: "crudangularfirebase-8b2dd.appspot.com",
+    messagingSenderId: "803725782465",
+    appId: "1:803725782465:web:e8ef6261c08a443aab1340",
+    measurementId: "G-X69RZVG2KE"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDGb5VA1pgtTKdPyLVWeiWFyqd9o75RJNM",
+    authDomain: "crudangularfirebase-8b2dd.firebaseapp.com",
+    databaseURL: "https://crudangularfirebase-8b2dd.firebaseio.com",
+    projectId: "crudangularfirebase-8b2dd",
+    storageBucket: "crudangularfirebase-8b2dd.appspot.com",
+    messagingSenderId: "803725782465",
+    appId: "1:803725782465:web:e8ef6261c08a443aab1340",
+    measurementId: "G-X69RZVG2KE"
+  }
 };
 
 /*
